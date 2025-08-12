30fabd9d8bdf2ec404b548ea101431029a45ba89
zayushiba07 <zayushiba07@gmail.com>
2025-08-12 19:06:04 +0530

    array_function

--- 8_array_function.php
+++ 8_array_function.php
@@ -46,4 +46,76 @@ echo "<br>";
 echo"======================================================";
 echo "<br>";
 print_r($cars);
+
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+$name = array("heni","rajesh","priti");
+$result = array_reverse($name);
+print_r($result);
+
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+$num1= array(50,90,30,10,20,30.50);
+echo array_sum($num1);
+
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+$num1= array(50,90,30,10,"ram",30.50);
+echo array_sum($num1);
+
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+$car = array("BMW","maruti","audi");
+$string = implode("-",$car);
+echo $string;
+
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+$pizza = "pieces1 pieces2 pieces3 pieces4";
+$array = explode(" ",$pizza);
+print_r($array);
+
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+$rank = array("fIrsT"=>1,"SecOnd"=>2,"ThiRD"=>3);
+print_r(array_change_key_case($rank,CASE_UPPER));
+
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+$rank = array("fIrsT"=>1,"SecOnd"=>2,"ThiRD"=>3);
+
+if(array_key_exists("first",$rank))
+{
+    echo "key found";   
+}
+else
+{
+    echo "key not found";
+}
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+$cars = array("BMW","Thar","Audi");
+array_unshift($cars,"i20");
+print_r($cars);
+echo "<br>";
+echo"======================================================";
+echo "<br>";
+
+
+echo rand(10,60);
 ?>
